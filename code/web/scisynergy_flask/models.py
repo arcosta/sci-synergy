dde6a22365f08f900d84fe628e71383cf91de627
arcosta <arcosta@gmail.com>
2017-09-08 14:44:43 -0300

    Try openshift database connection

--- code/web/scisynergy_flask/models.py
+++ code/web/scisynergy_flask/models.py
@@ -17,7 +17,8 @@ if os.environ.get('OPENSHIFT_PYTHON_IP') is not None:
 else:
     print('Using localhost database')
     #graph = Graph(hostname='datagraph.sci-synergy.svc', password='7474')
-    graph = Graph("http://hobby-bidcndeimgoagbkedjkhegkl.dbs.graphenedb.com:24789/db/data", user='openshiftuser', password='b.f7S3vbVb1bFO.wyLRsA6wEhkOjLUz')
+    #graph = Graph("http://hobby-bidcndeimgoagbkedjkhegkl.dbs.graphenedb.com:24789/db/data", user='openshiftuser', password='b.f7S3vbVb1bFO.wyLRsA6wEhkOjLUz')
+    graph = Graph("datagraph.sci-synergy.svc", user='neo4j', password='neo4j')
 
 #FIXME: Dont do this at home
 def fix_code(_str, code):
