1edac6151b4a730039e0782a5cb9777fe7f4a21d
Aurelio Costa <arcosta@gmail.com>
2018-06-13 22:39:19 -0300

    Implement basic teste

--- code/web/scisynergy_flask/tests/test_basic.py
+++ code/web/scisynergy_flask/tests/test_basic.py
@@ -1,9 +1,22 @@
 import os
 import unittest
  
-from scisynergy import app
+from scisynergy_flask import app
  
 class BasicTests(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
     def test_main_page(self):
         response = self.app.get('/', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
+
+    def test_maintenance(self):
+        response = self.app.get('/maintenance')
+
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == "__main__":
+    unittest.main()
