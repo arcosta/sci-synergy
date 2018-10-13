c5bf066ac479b58809cdbdad3af99c4c712d1667
Aurelio Costa <arcosta@gmail.com>
2018-10-12 21:49:02 -0300

    Code lint

--- code/web/scisynergy_flask/models.py
+++ code/web/scisynergy_flask/models.py
@@ -32,7 +32,7 @@ else:
 
 def verify_database():
     if len(graph) == 0:
-        raise "Empty Graph"
+        raise Exception("Empty Graph")
 
 
 class Researcher(object):
@@ -41,13 +41,17 @@ class Researcher(object):
     
     def __init__(self):
         self.publications = list()
+        self.name = ''
+        self.bagofareas = ''
+        self.lattesurl = ''
+        self.userid = ''
+        self.recomendation = list()
         if not Researcher.index_initialized:
             Researcher.indexOfNames = Researcher.initNamesIndex()        
     
     @staticmethod
     def tokenSanitize(token):
-        translationTable = None
-        if sys.version_info < (2,9):
+        if sys.version_info < (2, 9):
             import string 
             translationTable = string.maketrans("çàáâãéèêíóòöõüñ", "caaaaeeeiooooun")
             return token.lower().encode('utf-8').translate(translationTable)
@@ -57,7 +61,7 @@ class Researcher(object):
 
     @staticmethod
     def initNamesIndex():
-        invertedIndex = {}        
+        inverted_index = {}
         if graph == '':
             return None
         for author in graph.nodes.match("Author"):
@@ -75,59 +79,56 @@ class Researcher(object):
                     # Do not include short tokens
                     continue
                 token = Researcher.tokenSanitize(token)
-                if invertedIndex.get(token) is None:
-                    invertedIndex[token] = [authorid]
+                if inverted_index.get(token) is None:
+                    inverted_index[token] = [authorid]
                 else:
-                    invertedIndex[token].append(authorid)
+                    inverted_index[token].append(authorid)
                 
-        print("Index created with %i terms" %len(invertedIndex))
+        print("Index created with %i terms" % len(inverted_index))
         Researcher.index_initialized = True
-        return invertedIndex
+        return inverted_index
 
-        
-    def updateInfos(self, remoteNode):
+    def updateInfos(self, remote):
         """
         @desc Update user informations fetched from database
         """
-        self.name = remoteNode['name']
-        self.bagofareas = remoteNode['bagofareas']
-        self.lattesurl = remoteNode['lattesurl']
-        self.userid = remoteNode['userid']
+        self.name = remote['name']
+        self.bagofareas = remote['bagofareas']
+        self.lattesurl = remote['lattesurl']
+        self.userid = remote['userid']
         self.recomendation = list()
 
-        for rec in RelationshipMatch(graph, nodes=[remoteNode], r_type="RECOMMENDATION"):
-            recArea = rec.get('area')
+        for rec in RelationshipMatch(graph, nodes=[remote], r_type="RECOMMENDATION"):
+            rec_area = rec.get('area')
             
-            recStr = rec.end_node['name']
+            rec_str = rec.end_node['name']
             institution = ''            
-            if [x for x in self.recomendation if x.find(recStr) >= 0]:
+            if [x for x in self.recomendation if x.find(rec_str) >= 0]:
                 continue
             for assoc in rec.end_node.match_outgoing("ASSOCIATED TO"):
                 institution = assoc.end_node['name']
             if rec.end_node['PQ'] is not None:
-                recStr += " (" + rec.end_node['PQ'] + ")"
+                rec_str += " (" + rec.end_node['PQ'] + ")"
             if institution != '':
-                recStr += ' - <b>'+ institution.upper()+'</b>'
+                rec_str += ' - <b>' + institution.upper() + '</b>'
             
-            if recArea is not None:
-                recStr += " - Area: " + recArea
+            if rec_area is not None:
+                rec_str += " - Area: " + rec_area
                 
-            self.recomendation.append(recStr)
+            self.recomendation.append(rec_str)
 
-            
     def find(self, idx = 0):
         if idx is None or idx == 0:
             return None
         if graph == '':
             return None
-        retVal = None
+        retval = None
         for author in graph.nodes.match("Author"):
             if author['userid'] == int(idx):
-                retVal = author
+                retval = author
                 
-        if retVal is not None:
-            self.updateInfos(retVal)            
-            
+        if retval is not None:
+            self.updateInfos(retval)
         return self
     
     def findByName(self, name):
@@ -145,29 +146,26 @@ class Researcher(object):
 class Publication(object):
     def __init__(self):
         pass
-        
-        
+
     def find_by_author(author):
         publications = list()
         for rel in RelationshipMatch(graph, nodes=[author], r_type="AUTHORING"):
             (a,r,p) = rel.walk()
             publications.append(p)
         return publications
-        
-        
 
-    def colorcode(self, inst):
-        colormap = {'ufrn':'blue',
-                    'unb':  'green',
+    @staticmethod
+    def colorcode(inst):
+        colormap = {'ufrn': 'blue',
+                    'unb': 'green',
                     'ufam': 'gray',
                     'usp': 'red',
                     'ufmg': 'black'
                     }
         return colormap.get(inst, 'yellow')
-        
-        
+
     def relationCoauthoring(self, institution=None):
-        queryRelations = '''MATCH 
+        query_relations = '''MATCH 
     (a:Author)-[r1:AUTHORING]-(p:Publication)-[r2:AUTHORING]-(b:Author) 
 WHERE 
     p.type='article' AND a <> b 
@@ -178,47 +176,52 @@ OPTIONAL MATCH
 RETURN a, b, count(p) as c, i'''
         
         if institution is not None and institution != 'all':
-            queryRelations = '''MATCH 
+            query_relations = '''MATCH 
     (i:Institution {name: "%s"})-[r1:`ASSOCIATED TO`]-(a:Author)-[r2:AUTHORING]-(p:Publication)-[r3:AUTHORING]-(b:Author) 
 WHERE 
     p.type='article' 
 RETURN a, b, count(p) as c, i''' % institution
                     
-        relations = graph.run(queryRelations).data()
+        relations = graph.run(query_relations).data()
         links = list()
         if len(relations) < 10:
             raise Exception("Few nodes")
         for r in relations:
-            links.append({"source":r['a']["name"], 
-                          "target":r['b']["name"], 
-                          "size":6,
-                          "rel_count":r['c'],
-                          "institution": self.colorcode(r['i']['name']) if r['i'] is not None else 'yellow'
+            links.append({"source": r['a']["name"], 
+                          "target": r['b']["name"], 
+                          "size": 6,
+                          "rel_count": r['c'],
+                          "institution": Publication.colorcode(r['i']['name']) if r['i'] is not None else 'yellow'
                         })
         print("Modelo criado com %i arestas" % len(links))    
         return dict(children=links)
         
     def relationAuthorPublication(self):
-        queryRelations = "MATCH (a:Author)<-[r:AUTHORING]->(p:Publication) WHERE p.type='article' RETURN a,p"
-        relations = graph.run(queryRelations)
+        query_relations = "MATCH (a:Author)<-[r:AUTHORING]->(p:Publication) WHERE p.type='article' RETURN a,p"
+        relations = graph.run(query_relations)
             
         links = list()
         if relations.__len__() < 10:
             raise Exception("Few nodes")
         for r in relations:
-            links.append({"source":r[0]["name"], 
-                                    "target":r[1]["key"], 
-                                    "size":6}
-            )
+            links.append({"source": r[0]["name"],
+                          "target": r[1]["key"],
+                          "size": 6
+                          })
             
         return dict(children=links)
-    
+
+
 class Institution(object):
     def __init__(self):
         pass
-    def getInstitutionsName(self):        
+
+    @staticmethod
+    def getInstitutionsName():
         return [inst['name'] for inst in graph.nodes.match("Institution")]
-    def institutionInteraction(self):
+
+    @staticmethod
+    def institutionInteraction():
         query = '''MATCH path=(i1:Institution)-[]-(a1:Author)-[]-(p:Publication)-[]-(a2:Author)-[]-(i2:Institution)
                 WHERE id(i1) > id(i2)
                 RETURN i1.name,i2.name, count(p)
@@ -226,23 +229,27 @@ class Institution(object):
         interaction = graph.run(query)
         interlist = list()
         for i in interaction:
-            interlist.append({"source":i[0], "target":i[1], "rel_count":i[2]})
+            interlist.append({"source": i[0], "target": i[1], "rel_count": i[2]})
         return dict(result=interlist)
+
 
 class GraphInfo(object):
     def __init__(self):
         if type(graph) is str:
             raise RuntimeError("Graph not connected")
-    def nodeCount(self):
+
+    @staticmethod
+    def node_count():
         return graph.run("MATCH (n) RETURN count(n) AS nodes").data()[0]['nodes']
-        
-    def relCount(self):
+
+    @staticmethod
+    def rel_count():
         return graph.run("MATCH ()-[r]-() RETURN count(r) AS rels").data()[0]['rels']
         
-    def avgDegree(self):
-        numNodes = self.nodeCount()
-        numRels = self.relCount()
-        return numRels / numNodes
+    def avg_degree(self):
+        num_nodes = GraphInfo.node_count()
+        num_rels = GraphInfo.rel_count()
+        return num_rels / num_nodes
         
     def betweenness(self):
         pass
@@ -256,4 +263,3 @@ class GraphInfo(object):
     @lru_cache(maxsize=1024)    
     def distance(self, x, y):
         pass
-    
